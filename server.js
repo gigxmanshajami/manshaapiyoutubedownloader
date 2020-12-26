5b71b341eaf40e462cbb371fa8f2837fb588adee
Mansa Jami <mansajami2020@gmail.com>
2020-12-26 18:48:36 +0530

    YOUTUBE VIDEO DOWNLOADER MANSA JAMI (PRIVATE)

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
-
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 
+//enable ssl redirect
+app.use(sslRedirect());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'))
 app.listen(PORT, () => {
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/download', (req,res) => {
+app.get('/urldownload', (req,res) => {
 	var url = req.query.url;
 	res.header("Content-Disposition", 'attachment; filename="youtube-vid-mj.mp4');
 	ytdl(url, {format: 'mp4'}).pipe(res);
