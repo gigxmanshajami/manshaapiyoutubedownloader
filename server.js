67a082ef9ec782643dc9653a491e48c4148e916b
Mansa Jami <mansajami2020@gmail.com>
2021-01-09 20:57:57 +0530

    YOUTUBE VIDEO DOWNLOADE

--- server.js
+++ server.js
@@ -4,22 +4,25 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+const session = require('cookie-session');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'),
+	session({
+		secret: "YOUTUBE VIDEO DOWNLOADER",
+		httpOnly: true,
+		secure: true,
+	})
+);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
 
-app.get('*', (req, res) => {
-	// use secure connection
-    res.redirect('https://' + req.headers.host + req.url);
-});
 
 app.get('/', (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url);
     res.sendFile(__dirname + '/index.html');
 });
 app.get('/urldownload', (req,res) => {
