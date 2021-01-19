d7de820597790aef754928e958a0238d5b8a9951
Mansa Jami <mansajami2020@gmail.com>
2021-01-19 21:19:44 +0530

    YT DOWNLOADER

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-
 /* MANSA JAMI */
-
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const session = require('cookie-session');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public')
 
 );
-
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
-
-
 app.get('/', (req, res) => {
 	if (req.headers["x-forwarded-proto"] === "http") {
 		   res.redirect('https://' + req.headers.host + req.url);
@@ -25,6 +19,6 @@ app.get('/', (req, res) => {
 });
 app.get('/urldownload', (req,res) => {
 	var url = req.query.url;
-	res.header("Content-Disposition", 'attachment; filename="youtube-vid-mj.mp4');
+	res.header("Content-Disposition", 'attachment; filename="video-mj-yt.mp4');
 	ytdl(url, {format: 'mp4'}).pipe(res);
 });
