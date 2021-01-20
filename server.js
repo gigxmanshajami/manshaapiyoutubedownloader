78dc2123cc90f92bd03e3441f842ab33724c44fa
Mansa Jami <mansajami2020@gmail.com>
2021-01-20 15:40:02 +0530

    YOUTUBE VIDEO DOWNLOADER

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ytdl = require('ytdl-core');
 const session = require('cookie-session');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var nodemailer = require("nodemailer");
 app.use(express.static(__dirname + '/public')
 
 );
@@ -17,8 +18,38 @@ app.get('/', (req, res) => {
 	}
     res.sendFile(__dirname + '/index.html');
 });
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "freeappstoreint@gmail.com",
+        pass: "mansajami2020@gmail.com"
+    }
+});
+/*------------------SMTP Over-----------------------------*/
 app.get('/urldownload', (req,res) => {
 	var url = req.query.url;
 	res.header("Content-Disposition", 'attachment; filename="video-mj-yt.mp4');
 	ytdl(url, {format: 'mp4'}).pipe(res);
 });
+app.get('/send',function(req,res){
+    var mailOptions={
+    	name: req.query.name,
+        to : req.query.to,
+        from: req.query.from,
+        subject : req.query.subject,
+        text : `NAME = ${req.query.name} \n MESSAGE = ${req.query.text} \n EMAIL OF USER = ${req.query.from}`
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, res){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + res.message);
+           res.end("sent");
+         }
+});
+});
+
+/*--------------------Routing Over----------------------------*/
